a6349b828dfa72c3c6504e63bf3e4460bc60267f
Andriienko Yelyzaveta <yelyzaveta.andriienko@ukma.edu.ua>
2023-02-20 21:04:56 +0200

    Connected the React client to the Node.js server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
+
+app.use(cors())
 
 app.get("/", function(req, res) {
-  res.send("It's working!")
-})
+    res.send({"name": "Jane Doe"}) // Should be json format
+  })
 
 app.listen(3000, () => {
   console.log("app listening on port 3000")
