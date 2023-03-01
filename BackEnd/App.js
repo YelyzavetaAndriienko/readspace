d3bbb81d140620a75492984a48520098b8b96164
sun-ok <oleksandra.shliakhova@ukma.edu.ua>
2023-03-01 19:29:30 +0200

    Setup backend server

---
+++ BackEnd/App.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require("cors");
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const connectionString = "mongodb+srv://admin:admin@cluster0.tn0cl.mongodb.net/books-db?retryWrites=true&w=majority&authSource=admin";
+const mongoose = require('mongoose');
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.get("/", async(req, res) => {
+    return res.json("Books back End");;
+})
+
+app.listen(3001, function(err) {
+    console.log("Server listens");
+})
