ebecf1280b9d67c5aabe836858f124ac9af86c73
= <oleksandra.shliakhova@ukma.edu.ua>
2023-03-08 18:45:40 +0200

    Add back end to the registration form

--- BackEnd/controllers/UserControllers.js
+++ BackEnd/controllers/UserControllers.js
@@ -2,6 +2,10 @@ const bcrypt = require('bcrypt');
 const UserModer = require('../models/User')
 
 const postUser = async(req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
+    console.log(req.body)
     let user = getUser(req);
     user.save((err, userDB) => {
         if (err) {
